8766a3a4ac25638441eee1868342b85a228f53d8
Jamtech77 <cucumber6717@gmail.com>
2021-04-24 23:55:09 +0800

    Auto-save url to csv

--- selenium/search.py
+++ selenium/search.py
@@ -1,7 +1,7 @@
 from time import sleep
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import pandas as pd
 
 
 # --> 自動アクセスを実装してみる
@@ -24,10 +24,35 @@ sleep(3)
 height = 1000
 # Execute Javascript
 # driver.execute_script("window.scrollTo(x, y)")  
-while height < 10000:
+while height < 2000:
     driver.execute_script("window.scrollTo(0, {})".format(height))  
     height += 100
     sleep(1)
+
+# Get elements of images
+elements = driver.find_elements_by_class_name('sw-Thumbnail')
+
+d_list = []
+# Get URL from elements
+for i, element in enumerate(elements, start=1):
+    name = f'{query}_{i}'
+    yahoo_image_url = element.find_element_by_tag_name('img').get_attribute('src')
+    raw_url = element.find_element_by_class_name('sw-ThumbnailGrid__details').get_attribute('href')
+    title = element.find_element_by_tag_name('img').get_attribute('alt')
+
+    d = {
+        'filename': name,
+        'raw_url': raw_url,
+        'yahoo_image_url': yahoo_image_url,
+        'title': title
+    }
+
+    d_list.append(d)
+
+    sleep(2)
+
+df = pd.DataFrame(d_list)
+df.to_csv('image_urls.csv')
 
 sleep(2)
 driver.quit()
